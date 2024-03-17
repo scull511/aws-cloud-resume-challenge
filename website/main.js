aca8d7def98b7411ed2319c8fc9ebaa82af9b274
scull511 <simoncull511@hotmail.com>
2024-03-17 16:40:43 +0000

    same as last commit

--- website/main.js
+++ website/main.js
@@ -9,11 +9,11 @@ copyrightAndYearText.innerHTML = `&#169; Simon Cull ${year}`;
 const counterValue = document.querySelector("#counter-value");
 
 async function updateCounter() {
-  // let res = await fetch(
-  //   "https://rergvakopvnrdob2bi4cxo6ftu0ncujn.lambda-url.eu-west-2.on.aws/"
-  // );
-  // let value = await res.json();
-  let value = 12345;
+  let res = await fetch(
+    "https://rergvakopvnrdob2bi4cxo6ftu0ncujn.lambda-url.eu-west-2.on.aws/"
+  );
+  let value = await res.json();
+  
   counterValue.innerHTML = ` ${value.toLocaleString()}`;
 }
 
